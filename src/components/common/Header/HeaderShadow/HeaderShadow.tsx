37f0509c2793638173c463eec2b0ec815fb25b31
yuziwoo <yuziwoo@naver.com>
2024-02-14 15:32:20 +0900

    fix(HeaderShadow): 뒤로가기시 shadow가 적용되지 않던 오류 수정

--- src/components/common/Header/HeaderShadow/HeaderShadow.tsx
+++ src/components/common/Header/HeaderShadow/HeaderShadow.tsx
@@ -1,26 +1,29 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import * as S from './HeaderShadow.styled';
 
 const HeaderShadow = () => {
   // header shadow 효과 : 0 ~ 19px 스크롤 이동되면 opacity 0 ~ 0.95로 증가
-  const [shadowOpacity, setShadowOpacity] = useState(0);
   const SHADOW_EVENT_RANGE = 30;
   const MAX_SHADOW_OPACITY = 0.6;
-  const MAX_SHADOW_EVENT_RANGE = 120;
   const shadowStep = SHADOW_EVENT_RANGE / MAX_SHADOW_OPACITY;
+  const initialOpacity = -document.body.getBoundingClientRect().top / shadowStep;
+  const [shadowOpacity, setShadowOpacity] = useState(initialOpacity);
+
+  const handleShadowOpacity = useCallback(() => {
+    const top = -document.body.getBoundingClientRect().top;
+
+    if (top <= SHADOW_EVENT_RANGE) {
+      setShadowOpacity(top / shadowStep);
+      return;
+    }
+    if (shadowOpacity !== MAX_SHADOW_OPACITY) {
+      setShadowOpacity(MAX_SHADOW_OPACITY);
+    }
+
+    // eslint-disable-next-line
+  }, []);
 
   useEffect(() => {
-    const handleShadowOpacity = () => {
-      const top = -document.body.getBoundingClientRect().top;
-      if (top < MAX_SHADOW_EVENT_RANGE) {
-        if (top > SHADOW_EVENT_RANGE) {
-          setShadowOpacity(MAX_SHADOW_OPACITY);
-          return;
-        }
-        setShadowOpacity(top / shadowStep);
-      }
-    };
-
     handleShadowOpacity();
     window.addEventListener('scroll', handleShadowOpacity);
 
